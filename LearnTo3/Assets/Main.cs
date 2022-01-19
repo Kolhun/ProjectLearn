0c4951c2bdbd29008f952f7cd422390a557a44d3
Kolhun <36815965+Kolhun@users.noreply.github.com>
2022-01-19 12:56:01 +0300

    sum

---
+++ LearnTo3/Assets/Main.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    
+	public GameObject spawnItem;
+	public GameObject spawnItemParent;
+	private int spawnItemSize = 100;
+    
+    void Start()
+    {
+	    int j = 0;
+	    while(j <= spawnItemSize)
+	    {
+	    	var inst = Instantiate(spawnItem);
+	    	var jinst = inst.transform.GetChild(1);
+	    	jinst.GetComponent<Text>().text = "Элемент" + j.ToString();
+	    	inst.transform.SetParent(spawnItemParent.transform);
+	    	j++;
+	    }
+    }
+
+}
