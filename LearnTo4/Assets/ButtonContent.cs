07170bcb7f52c4f6e7fe1881842dc6306162ffa6
Kolhun <36815965+Kolhun@users.noreply.github.com>
2022-01-19 16:54:56 +0300

    lesson4

---
+++ LearnTo4/Assets/ButtonContent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonContent : MonoBehaviour
+{
+    
+	public string valueInput;
+    
+	public GameObject objParent;
+	public GameObject objChild;
+	public MainScript objMenu;
+	
+	public void ButtonExe()
+	{
+		
+		objMenu.sceneLoad = valueInput;
+		objParent.SetActive(false);
+		objChild.SetActive(true);
+
+	}
+	public void ButtonOut()
+	{	
+		
+		objParent.SetActive(true);
+		objChild.SetActive(false);
+
+	}
+	public void SceneLoad()
+	{
+		SceneManager.LoadScene(objMenu.sceneLoad);
+	}
+	public void SceneLoadMenu()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+    
+}
---
+++ LearnTo4/Assets/MainScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    
+	public GameObject objParent;
+	public GameObject objChild;
+	
+	public string sceneLoad;
+	
+	
+    
+    
+    
+}
