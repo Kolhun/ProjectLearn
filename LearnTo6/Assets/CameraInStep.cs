052569434d3033d4b05254eb09a6cad46172072e
Kolhun <36815965+Kolhun@users.noreply.github.com>
2022-03-04 16:09:39 +0300

    6745

---
+++ LearnTo6/Assets/CameraInStep.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraInStep : MonoBehaviour
+{
+    
+    public static CameraInStep main = new CameraInStep();
+    public Rigidbody rig;
+    public int speed = 4;
+
+    void Awake()
+    {
+        main = this;
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 vec = new Vector3(horizontal, 0, vertical);
+
+        rig.MovePosition(rig.position + vec * speed * Time.deltaTime);
+
+    }
+
+
+}
